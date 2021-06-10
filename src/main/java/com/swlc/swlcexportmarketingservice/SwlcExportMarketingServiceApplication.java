1af7b929ef570deb294a63a0d66a36657cc275c4
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-06-10 19:11:25 +0530

    spring starter commit

---
+++ src/main/java/com/swlc/swlcexportmarketingservice/SwlcExportMarketingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swlc.swlcexportmarketingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwlcExportMarketingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwlcExportMarketingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swlc/swlcexportmarketingservice/SwlcExportMarketingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swlc.swlcexportmarketingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwlcExportMarketingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
