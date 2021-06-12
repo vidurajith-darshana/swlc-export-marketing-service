f47dc1612b06c95e1b7415e7766fa0db5919a32b
Ramesh97 <hwr@1997>
2021-06-12 15:36:16 +0530

    Version 0.02 Application properties file update & rename customer entity to user

--- src/main/java/com/swlc/swlcexportmarketingservice/entity/User.java
+++ src/main/java/com/swlc/swlcexportmarketingservice/entity/User.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Customer {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int customerId;
