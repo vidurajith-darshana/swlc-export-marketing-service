67ea6f7016297f79efc2b54cc4b07667eb3006c4
Ramesh97 <hwr@1997>
2021-06-12 19:02:54 +0530

    Version 0.03 Updates Entity and Mappings

--- src/main/java/com/swlc/swlcexportmarketingservice/entity/Order.java
+++ src/main/java/com/swlc/swlcexportmarketingservice/entity/Order.java
@@ -1,6 +1,10 @@
 package com.swlc.swlcexportmarketingservice.entity;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "orders")
@@ -8,4 +12,22 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @NotNull
+    private double total;
+    @NotNull
+    private String message;
+    @NotNull
+    private String status;
+    @Column(name="create_date",insertable = false,updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createDate = new Date();
+
+    @ManyToOne
+    @JoinColumn(name = "customerId", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "productId",referencedColumnName = "productId")
+    private Product product;
 }
--- src/main/java/com/swlc/swlcexportmarketingservice/entity/Product.java
+++ src/main/java/com/swlc/swlcexportmarketingservice/entity/Product.java
@@ -1,13 +1,47 @@
 package com.swlc.swlcexportmarketingservice.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int productId;
+    @NotNull
+    private String productName;
+    @NotNull
+    private String productCode;
+    @NotNull
+    private String productThumbnail;
+    @NotNull
+    private Double productPrice;
+    @NotNull
+    private String productStatus;
+    @NotNull
+    private int productTotalQty;
+    @NotNull
+    private int productCurrentQty;
+    @Column(name="create_date",insertable = false,updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createDate = new Date();
+
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private List<Order> orders = new ArrayList<>();
+
+    public Product(int productId) {
+        this.productId = productId;
+    }
 }
