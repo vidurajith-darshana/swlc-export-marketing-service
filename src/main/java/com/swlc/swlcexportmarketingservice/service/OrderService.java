e77d6035fba699a0c2d547780736f7b87c52e07a
Ramesh97 <hwr@1997>
2021-06-13 09:18:06 +0530

    Version 0.04 order Service

---
+++ src/main/java/com/swlc/swlcexportmarketingservice/service/OrderService.java
@@ -0,0 +1,4 @@
+package com.swlc.swlcexportmarketingservice.service;
+
+public interface OrderService {
+}
---
+++ src/main/java/com/swlc/swlcexportmarketingservice/service/impl/OrderServiceImpl.java
@@ -0,0 +1,4 @@
+package com.swlc.swlcexportmarketingservice.service.impl;
+
+public class OrderServiceImpl {
+}
