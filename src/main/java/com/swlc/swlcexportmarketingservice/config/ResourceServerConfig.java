c77d98f2d728db49ee50571a164d431156ff7db8
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-06-22 10:23:19 +0530

    change: Apply restrictions

--- src/main/java/com/swlc/swlcexportmarketingservice/config/ResourceServerConfig.java
+++ src/main/java/com/swlc/swlcexportmarketingservice/config/ResourceServerConfig.java
@@ -21,11 +21,22 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+
+                // Admin
                 .antMatchers("/api/v1/user/operator/create").hasAuthority(ApplicationConstant.USER_ROLES.ROLE_ADMIN.toString())
+                .antMatchers("/api/v1/admin/category/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+                .antMatchers("/api/v1/admin/product/**").access("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
+
+                // Registered User(Customer)
                 .antMatchers("/api/v1/user/customer/create").permitAll()
-//                .antMatchers("/api/v1/admin/**").access("hasAuthority('ROLE_ADMINISTRATOR')")
                 .antMatchers("/api/v1/user/**").access("hasAuthority('ROLE_USER')")
+                .antMatchers("/api/order/**").access("hasAuthority('ROLE_USER')")
+
+                // Any User
+                .antMatchers("/api/v1/user/category/**").permitAll()
+                .antMatchers("/api/v1/user/product/**").permitAll()
                 .antMatchers("/**").permitAll()
+
                 .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
         http.csrf().disable();
     }
