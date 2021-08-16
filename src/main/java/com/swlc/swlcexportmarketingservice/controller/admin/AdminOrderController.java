e39e96eda1d84166316fc77a9f9c7fabb8c50285
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-08-16 17:21:04 +0530

    create admin order controller

---
+++ src/main/java/com/swlc/swlcexportmarketingservice/controller/admin/AdminOrderController.java
@@ -0,0 +1,16 @@
+package com.swlc.swlcexportmarketingservice.controller.admin;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author kavindu
+ */
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/admin/order")
+public class AdminOrderController {
+
+}
