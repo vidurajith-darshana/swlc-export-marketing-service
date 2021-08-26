740b09d7ef29726347f0e0e45e394eec27dfa571
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-08-26 07:35:18 +0530

    fix bug at customer reviews

--- src/main/java/com/swlc/swlcexportmarketingservice/service/CustomerReviewService.java
+++ src/main/java/com/swlc/swlcexportmarketingservice/service/CustomerReviewService.java
@@ -1,10 +1,11 @@
 package com.swlc.swlcexportmarketingservice.service;
 
+import com.swlc.swlcexportmarketingservice.dto.CustomerReviewSaveRequestDTO;
 import com.swlc.swlcexportmarketingservice.dto.CustomerReviewsDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface CustomerReviewService {
-    boolean saveNewReview(CustomerReviewsDto dto);
+    boolean saveNewReview(CustomerReviewSaveRequestDTO dto);
     Page<CustomerReviewsDto> getCustomerReviews(int year, int month, Pageable pageable);
 }
